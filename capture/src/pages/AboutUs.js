56b74ad76f6bfad9f2d2e6fbcb957de176f32cee
stjeandan-swc <stjean.dan.dev@gmail.com>
2021-01-28 15:11:06 -0500

    started the "About Us" Section

---
+++ capture/src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// pages components
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+    return(
+        <div>
+            <AboutSection />
+        </div>
+    );
+}
+
+export default AboutUs;
