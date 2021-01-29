1696eb14a32f91be5aefdcc87596ebbd3febcb20
stjeandan-swc <stjean.dan.dev@gmail.com>
2021-01-29 10:28:26 -0500

    setup FAQ section layout

---
+++ capture/src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return(
+        <div className='faq'>
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>how Do I Start?</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Voluptatibus, nam.
+                </p>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule.</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Voluptatibus, nam.
+                </p>
+            </div>
+            <div className="question">
+                <h4>Methods of payment.</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Voluptatibus, nam.
+                </p>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer?</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Voluptatibus, nam.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
