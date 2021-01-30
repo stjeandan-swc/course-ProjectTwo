08d85df53aee3662e6e1eacbd968e905010eeb73
stjeandan-swc <stjean.dan.dev@gmail.com>
2021-01-30 13:55:47 -0500

    setup layout of our work page and added styling

--- capture/src/pages/OurWork.js
+++ capture/src/pages/OurWork.js
@@ -1,11 +1,66 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import {Link} from 'react-router-dom';
+
+// images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
+
 const OurWork = () => {
     return(
-        <div>
-            <h1>Our Work</h1>
-        </div>
+        <STYLE_WORK>
+            <STYLE_MOVIE>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete"/>
+                </Link>
+            </STYLE_MOVIE>
+            <STYLE_MOVIE>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer"/>
+                </Link>
+            </STYLE_MOVIE>
+            <STYLE_MOVIE>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes"/>
+                </Link>
+            </STYLE_MOVIE>
+        </STYLE_WORK>
     );
 };
+
+const STYLE_WORK = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+
+    h2{
+        padding: 1rem 0rem;
+    }
+`;
+
+const STYLE_MOVIE = styled.div`
+    padding-bottom: 10rem;
+
+    .line{
+        height: 0.5rem;
+        background: #ccc;
+        margin-bottom: 3rem;
+    }
+
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`;
 
 export default OurWork;
